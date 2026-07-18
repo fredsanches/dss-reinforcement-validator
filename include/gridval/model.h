8c764b67adb201a43aa095dd64af69fa1d15956f
Frederico Sanches <fredericosanches@hotmail.com>
2026-07-18 13:50:57 -0300

    chore: implement model.h from step 3, task 2.

--- include/gridval/model.h
+++ include/gridval/model.h
@@ -6,6 +6,66 @@
 #define GRIDVAL_ID_SIZE 64
 #define GRIDVAL_SCENARIO_NAME_SIZE 128
 
+typedef struct
+{
+  char id[GRIDVAL_ID_SIZE];
+  double voltage_pu;
+} Bus;
 
+typedef struct 
+{
+  char id[GRIDVAL_ID_SIZE];
+  double loading_percent;
+} Line;
+
+typedef struct
+{
+  char id[GRIDVAL_ID_SIZE];
+  double loading_percent;
+} Transformer;
+
+typedef struct
+{
+  Bus *items;
+  size_t count;
+  size_t capacity;
+} BusArray;
+
+typedef struct
+{
+  Line *items;
+  size_t count;
+  size_t capacity;
+} LineArray;
+
+typedef struct
+{
+  Transformer *items;
+  size_t count;
+  size_t capacity;
+} TransformerArray;
+
+typedef struct
+{
+  char name[GRIDVAL_SCENARIO_NAME_SIZE];
+  BusArray buses;
+  LineArray lines;
+  TransformerArray transformers;
+} Scenario;
+
+int bus_array_init(BusArray *array);
+int bus_array_append(BusArray *array, Bus value);
+void bus_array_free(BusArray *array);
+
+int line_array_init(LineArray *array);
+int line_array_append(LineArray *array, Line value);
+void line_array_free(LineArray *array);
+
+int transformer_array_init(TransformerArray *array);
+int transformer_array_append(TransformerArray *array, Transformer value);
+void transformer_array_free(TransformerArray *array);
+
+int scenario_init(Scenario *scenario);
+void scenario_free(Scenario *scenario);
 
 #endif
